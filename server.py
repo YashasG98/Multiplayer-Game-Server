4b7c109019748dd83b9e6b39dde405cdd776e76d
YashasG98 <yashu191298@gmail.com>
2019-10-20 10:34:26 +0530

    added login functionality

--- server.py
+++ server.py
@@ -5,27 +5,16 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'Game_server'
-app.config['MYSQL_PASSWORD'] = 'Shashwath@99'
+app.config['MYSQL_PASSWORD'] = 'Game_server'
+# app.config['MYSQL_PASSWORD'] = 'Shashwath@99'
 app.config['MYSQL_DB'] = 'Game_server'
 
 mysql = MySQL(app)
 
 @app.route('/')
 def Home():
-    # cur = mysql.connection.cursor()
-    # cur.execute("SELECT * FROM users where name like '%o%';")
-    # fetchdata = cur.fetchall()
-    # cur.close()
     return render_template('login.html')
 
-@app.route('/login.html')
-def Login():
-    # cur = mysql.connection.cursor()
-    # cur.execute("SELECT * FROM users where name like '%o%';")
-    # fetchdata = cur.fetchall()
-    # cur.close()
-    return render_template('login.html')
 
 @app.route('/register.html',methods = ['GET','POST'])
 def Register():
@@ -50,6 +39,35 @@ def Register():
         else:
             error = 'Email already registered!'
     return render_template('register.html', error = error)
+
+@app.route('/login.html', methods = ['GET', 'POST'])
+def Login():
+    error = None
+
+    if request.method=='POST':
+        email=request.form['loginEmail']
+        password=request.form['loginPassword']
+        cur=mysql.connection.cursor()
+        _sql = "select md5('{0}')"
+        cur.execute(_sql.format(password))
+        enc_string=cur.fetchall()
+        _sql = "select password from Login_credentials where email = '{0}'"
+        cur.execute(_sql.format(email))
+        stored=cur.fetchall()
+        if(len(stored) is 0):
+            error = 'Email not found!'
+        else:
+            if(enc_string==stored):
+                return redirect('/index.html', code=302)
+                cur.close()
+            else:
+                error = 'Invalid password'
+
+    return render_template('login.html', error = error)
+
+@app.route('/index.html')
+def Index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
